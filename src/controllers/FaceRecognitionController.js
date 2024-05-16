87dfebdaf8221aeb6a1e9f1c6cc0dfc8b617b5f8
Gabriel Valmarath <melovalmarath.gabriel@gmail.com>
2024-05-15 21:15:25 -0300

    Update - Changed face recognition library, from face-api to human; also added first successful face comparison/match

--- src/controllers/FaceRecognitionController.js
+++ src/controllers/FaceRecognitionController.js
@@ -1,62 +1,79 @@
-const tf = require('@tensorflow/tfjs-node')
-const faceapi = require('@vladmandic/face-api');
-const fs = require('fs');
+const tf = require('@tensorflow/tfjs-node'); // can also use '@tensorflow/tfjs-node-gpu' if you have environment with CUDA extensions
+const Human = require('@vladmandic/human').default; // points to @vladmandic/human/dist/human.node.js
+const fs = require('fs')
 
-const { faceDetectionNet, faceDetectionOptions } = require("./commons/faceDetection")
-const saveFile = require("./commons/saveFile")
-//const canvas = require("./commons/env")
+const myConfig = {
+    face: {
+    enabled: true,
+    detector: { rotation: true, return: true },
+    mesh: { enabled: true },
+    description: { enabled: true },
+    },
+};
 
-const REFERENCE_IMAGE = fs.readFileSync('./src/images/vic1.JPG')
-const QUERY_IMAGE = fs.readFileSync('./src/images/perfil.JPG')
+const human = new Human(myConfig);
 
-const readImage = path => {
-  //reads the entire contents of a file.
-  //readFileSync() is synchronous and blocks execution until finished.
-  const imageBuffer = fs.readFileSync(path);
-  //Given the encoded bytes of an image,
-  //it returns a 3D or 4D tensor of the decoded image. Supports BMP, GIF, JPEG and PNG formats.
-  const tfimage = tfnode.node.decodeImage(imageBuffer);
+const QUERY_IMAGE = fs.readFileSync('./src/images/perfil.jpg')
+const QUERY_IMAGE2 = fs.readFileSync('./src/images/profile.jpg')
+const REFERENCE_IMAGE = fs.readFileSync('./src/images/pyBR.jpeg')
+const REFERENCE_IMAGE2 = fs.readFileSync('./src/images/IMG_5665.JPG')
+
+const readImage = (buffer) => {     
+  const tfimage = tf.node.decodeImage(buffer);
   return tfimage;
- }
+}
 
 const test = async (req, res) => {
 
-  await faceDetectionNet.loadFromDisk('./src/weights')
-  await faceapi.nets.faceLandmark68Net.loadFromDisk('./src/weights')
-  await faceapi.nets.faceRecognitionNet.loadFromDisk('./src/weights')
-  
-  const referenceImage = tf.node.decodeImage(REFERENCE_IMAGE)
-  const queryImage = tf.node.decodeImage(QUERY_IMAGE)
-  
-  const resultsRef = await faceapi.detectAllFaces(referenceImage, faceDetectionOptions)
-    .withFaceLandmarks()
-    .withFaceDescriptors()
-  const resultsQuery = await faceapi.detectAllFaces(queryImage, faceDetectionOptions)
-    .withFaceLandmarks()
-    .withFaceDescriptors()
+    try {
+        const detectQuery1 = human.detect(readImage(QUERY_IMAGE));
+        const detectQuery2 = human.detect(readImage(QUERY_IMAGE2));
+    
+        const detectRef1 = human.detect(readImage(REFERENCE_IMAGE));
+        const detectRef2 = human.detect(readImage(REFERENCE_IMAGE2));
+    
+        const [query1, query2, ref1, ref2] = await Promise.all([detectQuery1, detectQuery2, detectRef1, detectRef2]);
+    
+        //const reqImage = await human.detect(readImage(req.file.buffer))
+    
+        const find1Promise = new Promise((resolve, reject) => {
+            const find1 = human.match.find(query1.face[0] ? query1.face[0].embedding : [], [ref1.face[0].embedding, ref2.face[0].embedding]);
+            resolve(find1);
+        });
+        
+        const find2Promise = new Promise((resolve, reject) => {
+            const find2 = human.match.find(query2.face[0] ? query2.face[0].embedding : [], [ref1.face[0].embedding, ref2.face[0].embedding]);
+            resolve(find2);
+        });
+    
+        Promise.all([find1Promise, find2Promise])
+            .then(results => {
+                // Handle results
+                if(results.length > 0) {
+                    
+                    let biggestMatch = results[0];
 
-  //console.log(resultsQuery)
-  const faceMatcher = new faceapi.FaceMatcher(resultsRef)
+                    for (let i = 0; i < results.length; i++) {
+                        if (results[i].similarity > biggestMatch.similarity) {
+                            biggestMatch = results[i]; 
+                        }
+                    }
 
-  const labels = faceMatcher.labeledDescriptors
-    .map(ld => ld.label)
-  const refDrawBoxes = resultsRef
-    .map(res => res.detection.box)
-    .map((box, i) => new faceapi.draw.DrawBox(box, { label: labels[i] }))
-  //const outRef = faceapi.createCanvasFromMedia(referenceImage)
-  //refDrawBoxes.forEach(drawBox => drawBox.draw(outRef))
+                    return res.status(200).json({ result: biggestMatch.similarity*100 });
 
-  //saveFile('referenceImage.jpg', outRef.toBuffer('image/jpeg'))
+                } else {
+                    return res.status(200).json({ message: 'No faces were detected' });
+                }
+            })
+            .catch(err => {
+                // Handle errors
+            return res.status(500).json({ Error: err });
+        });
+    
+    } catch (err) {
+        return res.status(500).json({ Error: err });
+    }
 
-  const queryDrawBoxes = resultsQuery.map(res => {
-    const bestMatch = faceMatcher.findBestMatch(res.descriptor)
-    return new faceapi.draw.DrawBox(res.detection.box, { label: bestMatch.toString() })
-  })
-  //const outQuery = faceapi.createCanvasFromMedia(queryImage)
-  //queryDrawBoxes.forEach(drawBox => drawBox.draw(outQuery))
-  //saveFile('queryImage.jpg', outQuery.toBuffer('image/jpeg'))
-  console.log('done, saved results to out/queryImage.jpg')
-  return res.status(200).json({ result: queryDrawBoxes });
 }
 
 module.exports = { test };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+const multer = require('multer');
 
 const FaceRecognitionController = require('../controllers/FaceRecognitionController.js');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const router = Router();
 
 router.get('/ping', (req, res) => res.json({ pong: true }));
 
-router.get('/test', FaceRecognitionController.test);
+router.post('/test', upload.single('data'), FaceRecognitionController.test);
 
 // Login and Register
 //router.post('/register', AuthValidator.register, AuthController.register)
