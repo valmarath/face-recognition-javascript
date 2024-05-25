e3bd49956d9212c2beebfe92392053e0473a3d28
Gabriel Valmarath <melovalmarath.gabriel@gmail.com>
2024-05-25 01:35:10 -0300

    FIX - changed multer storage and found bug when using .find

--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -30,7 +30,7 @@ const readImage = (buffer) => {
 const login = async (req, res) => {
 
     try {
-        const user = await pool.query('SELECT * FROM "USERS" WHERE username = $1', [req.body.username]);
+        const user = await pool.query('SELECT user_username, user_password FROM "USERS_VIEW" WHERE user_username = $1', [req.body.username]);
 
         if(user.rowCount == 0) {
             res.status(401);
@@ -38,7 +38,7 @@ const login = async (req, res) => {
             return;
         }
     
-        const passwordMatch = await bcrypt.compare(req.body.password, user.rows[0].password);
+        const passwordMatch = await bcrypt.compare(req.body.password, user.rows[0].user_password);
         
         if(!passwordMatch) {
             res.status(401);
@@ -58,9 +58,10 @@ const login = async (req, res) => {
 
 const faceLogin = async (req, res) => {
 
-    try {
 
         const user = await pool.query('SELECT * FROM "USERS" WHERE username = $1', [req.body.username]);
+        const userView = await pool.query('SELECT * FROM "USERS_VIEW" WHERE user_username = $1', [req.body.username]);
+        //console.log(userView)
 
         if(!user) {
             return res.status(401).json({ error: "Incorrect e-mail or face didn't match!" });
@@ -69,20 +70,32 @@ const faceLogin = async (req, res) => {
         const detectQuery1 = human.detect(readImage(QUERY_IMAGE));
         const detectQuery2 = human.detect(readImage(QUERY_IMAGE2));
     
-        const detectRef1 = human.detect(readImage(REFERENCE_IMAGE));
-        const detectRef2 = human.detect(readImage(REFERENCE_IMAGE2));
-    
-        const detectReqImage = human.detect(readImage(req.file.buffer))
+        const detectRef1 = await human.detect(readImage(REFERENCE_IMAGE));
+        const detectRef2 = await human.detect(readImage(REFERENCE_IMAGE2));
+            
+        const queryImage = fs.readFileSync(req.file.path);
 
+        const detectReqImage = human.detect(readImage(queryImage));
+        
         //const [query1, query2, ref1, ref2] = await Promise.all([detectQuery1, detectQuery2, detectRef1, detectRef2]);
         const [reqImage, ref1, ref2] = await Promise.all([detectReqImage, detectRef1, detectRef2]);
-    
-        const find1Promise = new Promise((resolve, reject) => {
-            const find1 = human.match.find(reqImage.face[0] ? reqImage.face[0].embedding : [], [ref1.face[0].embedding, ref2.face[0].embedding]);
-            resolve(find1);
+        
+        const reqImage2 = await human.detect(readImage(queryImage))
+
+        fs.unlink(req.file.path, (err) => {
+            if (err) {
+              console.error('Error deleting file:', err);
+            }
         });
 
-/*         const find1Promise = new Promise((resolve, reject) => {
+
+        // A princípio usar similarity em paralelo, validar com promises e enfim
+        // match.find mostrou resultados variáveis estranhos     
+        // Avaliar promises e awaits, algo gerando resultados errados (da forma que está, funciona)   
+        const find1Promise = await human.match.similarity(reqImage2.face[0] ? reqImage2.face[0].embedding : [], detectRef1.face[0].embedding);
+
+        console.log(find1Promise)
+        /*         const find1Promise = new Promise((resolve, reject) => {
             const find1 = human.match.find(query1.face[0] ? query1.face[0].embedding : [], [ref1.face[0].embedding, ref2.face[0].embedding]);
             resolve(find1);
         });
@@ -93,7 +106,7 @@ const faceLogin = async (req, res) => {
         }); */
     
         //Promise.all([find1Promise, find2Promise])
-        Promise.all([find1Promise])
+        await Promise.all([find1Promise])
             .then(results => {
                 // Handle results
                 if(results.length > 0) {
@@ -110,7 +123,7 @@ const faceLogin = async (req, res) => {
 
                     if(biggestMatch.similarity > 0.6) {
                         const token = generateToken({id: user.rows[0].id});
-                        return res.status(200).json({ token: token });
+                        return res.status(200).json({ token: token, array: ref1.face[0].embedding, array2: ref2.face[0].embedding });
                     } else {
                         return res.status(401).json({ error: "Incorrect e-mail or face didn't match!" });
                     }
@@ -123,9 +136,7 @@ const faceLogin = async (req, res) => {
             return res.status(500).json({ Error: err });
         });
     
-    } catch (err) {
-        return res.status(500).json({ Error: err });
-    }
+
 }
 
 module.exports = { 
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -3,8 +3,28 @@ const multer = require('multer');
 
 const AuthController = require('../controllers/AuthController.js');
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './src/tmp');
+    },
+    filename: (req, file, cb) => {
+        let randomName = Math.floor(Math.random() * 99999999999);
+        cb(null, `${randomName}.jpg`)
+    }
+});
+
+const upload = multer({ 
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        const allowed = ['image/jpg', 'image/jpeg', 'image/png'];
+        if (allowed.includes(file.mimetype)) {
+            cb(null, true);
+          } else {
+            cb(new Error("Invalid file type."));
+          }
+    },
+    limits: { fieldNameSize: 200, fieldSize: 2000000},
+});
 
 const router = Router();
 
